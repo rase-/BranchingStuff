e93a9854b97bb3543fb142cd10c85ba767a291a9
Tony Kovanen <tonykovanen@hotmail.com>
2012-03-19 16:31:36 +0200

    kansiojakoa muutettu

---
+++ viikko2/XML/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,15 @@
+package ohtu.verkkokauppa;
+
+public class Viitegeneraattori implements Viitteitageneroiva {
+
+    
+    private int seuraava;
+    
+    public Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    public int uusi(){
+        return seuraava++;
+    }
+}
