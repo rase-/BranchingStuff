78f7b7782874c17c06df2a449890430c74c09f81
Tony Kovanen <tonykovanen@hotmail.com>
2012-03-25 18:14:40 +0300

    t5 viikko 3

--- viikko2/Annotaatio/src/main/java/ohtu/Main.java
+++ viikko2/Annotaatio/src/main/java/ohtu/Main.java
@@ -30,5 +30,83 @@ public class Main {
         for (String tapahtuma : kauppa.getVarasto().getKirjaapitava().getTapahtumat()) {
             System.out.println(tapahtuma);
         }
+				for (int i = 0; i < 10; i++) {
+					for(int k =0 ; k < 10; k++) {
+					for (int j = 0; j < 10; j++) {
+						System.out.println("Naminami");
+					}
+					}
+				}
+			if (0 == 1) {
+				if (1 == 0) {
+					if (2==3) {
+						System.out.println("Numinumi");
+					}
+				}
+			}
+			System.out.println("Naminami");
+			System.out.println("Naminami");
+			System.out.println("Naminami");
+			System.out.println("Naminami");
+			System.out.println("Naminami");
+			System.out.println("Naminami");
+			System.out.println("Naminami");
     }
-}
+	public static void metodi() {
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}if (2==0) {
+			
+		}if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}if (2==0) {
+			
+		}if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+		if (2==0) {
+			
+		}
+	}}
